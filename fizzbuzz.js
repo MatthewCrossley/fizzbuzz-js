67a86b346f3c51c4fe4d420586e5f3272841b645
Matthew Crossley <matthew.crossley@bp.com>
2022-09-28 12:26:22 +0100

    Add Fezz and reverse rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,32 +6,49 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (var i = 1; i < 101; i++){
+    for (var i = 1; i < 201; i++){
         var fizz = i % 3 == 0
         var buzz = i % 5 == 0
         var bang = i % 7 == 0
         var bong = i % 11 == 0
-        var out = ""
+        var fezz = i % 13 == 0
+        var reverse = i % 17 == 0
+        var out = []
 
         if (fizz){
-            out += "Fizz"
+            out.push("Fizz")
         }
         if (buzz){
-            out += "Buzz"
+            out.push("Buzz")
         }
         if (bang) {
-            out += "Bang"
+            out.push("Bang")
         }
         if (bong){
             // bong overrides everything else
-            // so don't += here
-            out = "Bong"
+            // so don't push here
+            out = ["Bong"]
+        }
+        if (fezz){
+            for (var j = 0; j < out.length; j++){
+                if (out[j].indexOf("B") === 0){
+                    out.splice(j, 0, "Fezz")
+                    break
+                }
+            }
+            if (out.indexOf("Fezz") === -1){
+                out.push("Fezz")
+            }
         }
 
-        if (out === ""){
+        if (reverse){
+            out.reverse()
+        }
+
+        if (out.length === 0){
             console.log(i)
         } else {
-            console.log(out)
+            console.log(out.join(""))
         }
     }
     
