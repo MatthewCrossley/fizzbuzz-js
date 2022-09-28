c0df2b7161186a92a7553a90ffc00f613fb265aa
Matthew Crossley <matthew.crossley@bp.com>
2022-09-28 14:02:45 +0100

    Add command line args.
    Max number to print up to can be specified via the CLI, every fizz/buzz/bang/...
    divisor can also be specified

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,44 @@
 const prompt = require("prompt-sync")();
 
+function parseArg(arg, rules){
+    for (key in rules){
+        if (arg.indexOf(`${key}=`) === 0){
+            rules[key] = Number(arg.replace(`${key}=`, ""))
+            return
+        }
+    }
+}
+
 // Here, we create our main function.
 function fizzbuzz() {
-    
+    var rules = {
+        "fizz": 3,
+        "buzz": 5,
+        "bang": 7,
+        "bong": 11,
+        "fezz": 13,
+        "reverse": 17,
+        "maxNum": -1
+    }
+
+    // process each arg into rules dict
+    process.argv.slice(2).forEach(a => parseArg(a, rules))
+
     console.log('Welcome to FizzBuzz!');
 
-    const input = prompt("Max number to print up to: ")
-    var maxNum = Number(input)
+    if (rules["maxNum"] === -1){
+        const input = prompt("Max number to print up to: ")
+        rules["maxNum"] = Number(input)
+    }
 
     // Put your code here...
-    for (var i = 1; i < maxNum; i++){
-        var fizz = i % 3 == 0
-        var buzz = i % 5 == 0
-        var bang = i % 7 == 0
-        var bong = i % 11 == 0
-        var fezz = i % 13 == 0
-        var reverse = i % 17 == 0
+    for (var i = 1; i < rules["maxNum"]; i++){
+        var fizz = i % rules["fizz"] == 0
+        var buzz = i % rules["buzz"] == 0
+        var bang = i % rules["bang"] == 0
+        var bong = i % rules["bong"] == 0
+        var fezz = i % rules["fezz"] == 0
+        var reverse = i % rules["reverse"] == 0
         var out = []
 
         if (fizz){
