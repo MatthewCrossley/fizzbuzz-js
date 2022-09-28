fa70c367e20d84639837cf91fa015fc51bf09eea
Crossley <matthew.crossley@bp.com>
2022-09-28 12:01:18 +0100

    Add Fizz, Buzz, FizzBuzz, Bang and Bong rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,34 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
+    for (var i = 1; i < 101; i++){
+        var fizz = i % 3 == 0
+        var buzz = i % 5 == 0
+        var bang = i % 7 == 0
+        var bong = i % 11 == 0
+        var out = ""
 
+        if (fizz){
+            out += "Fizz"
+        }
+        if (buzz){
+            out += "Buzz"
+        }
+        if (bang) {
+            out += "Bang"
+        }
+        if (bong){
+            // bong overrides everything else
+            // so don't += here
+            out = "Bong"
+        }
+
+        if (out === ""){
+            console.log(i)
+        } else {
+            console.log(out)
+        }
+    }
     
 }
 
